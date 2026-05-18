2ab94577f68dd82ec75f911d6c45247ff03e33a0
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2026-05-18 23:49:21 +0200

    Add akplanning service module

--- modules/akplanning.nix
+++ modules/akplanning.nix
@@ -1,146 +1,330 @@
 {
-  lib,
+  self,
   ...
 }:
 
 {
-  perSystem =
+  flake.nixosModules.akplanning =
     {
-      self',
+      lib,
+      config,
       pkgs,
       ...
     }:
 
     let
-      src = pkgs.fetchgit {
-        url = "https://gitlab.fachschaften.org/kif/akplanning.git";
-        rev = "faf3e534998f67e315cfa0f9c5d21b87c032a632";
-        hash = "sha256-rmnxA2xj6xXUebzottnzkaAF091KaT2b1Pwi2frGZQ4=";
-      };
+      inherit (lib)
+        mkOption
+        mkEnableOption
+        mkPackageOption
+        mkIf
+        types
+        ;
 
-      python = pkgs.python3Packages.python.override {
-        packageOverrides = self: super: {
-          django = super.django_5;
-
-          inherit (self'.packages)
-            django-admin-logs
-            django-betterforms
-            django-bootstrap-datepicker-plus
-            django-docs
-            django-fontawesome-6
-            django-tex
-            django-registration-redux
-            sphinxcontrib-django
-            ;
-        };
-      };
-
-      texlive = pkgs.texlive.combine {
-        inherit (pkgs.texlive.pkgs)
-          collection-basic
-          collection-luatex
-          collection-latex
-          collection-latexrecommended
-          collection-latexextra
-          collection-fontsrecommended
-          collection-fontsextra
-          collection-langgerman
-          beamer
-          ;
-      };
-
-      path = lib.makeBinPath [
-        texlive
-      ];
-
-      dependencies = lib.attrValues {
-        inherit (python.pkgs)
-          django
-          django-admin-logs
-          django-betterforms
-          django-bootstrap-datepicker-plus
-          django-bootstrap5
-          django-compressor
-          django-debug-toolbar
-          django-fontawesome-6
-          django-libsass
-          django-registration-redux
-          django-simple-history
-          django-split-settings
-          django-tex
-          django-timezone-field
-          django-csp
-          djangorestframework
-
-          fontawesomefree
-          mysqlclient
-          tzdata
-          jsonschema
-
-          sphinx
-          sphinx-rtd-theme
-          sphinxcontrib-apidoc
-          sphinxcontrib-django
-          recommonmark
-          django-docs
-          ;
-      };
+      pythonFmt = pkgs.formats.pythonVars { };
     in
     {
-      packages.akplanning = python.pkgs.buildPythonApplication {
-        name = "akplanning";
-        version = "0-unstable-2026-05-18";
-        pyproject = false;
-        inherit src;
+      options.die-koma.akplanning = {
+        enable = mkEnableOption "configure the AKPlanning tool";
 
-        nativeBuildInputs = [
-          pkgs.gettext
-        ];
+        package = mkPackageOption self.packages."${pkgs.stdenv.hostPlatform.system}" "akplanning" { };
 
-        inherit dependencies;
-
-        buildPhase = ''
-          runHook preBuild
-
-          mkdir -p $out/lib
-          cp -r $src $out/lib/akplanning
-          chmod -R +w $out/lib/akplanning
-          rm -rf $out/lib/akplanning/docs
-
-          runHook postBuild
-        '';
-
-        installPhase =
-          let
-            pythonPath = python.pkgs.makePythonPath dependencies;
-          in
-          ''
-            runHook preInstall
-
-            chmod +x $out/lib/akplanning/manage.py
-            makeWrapper $out/lib/akplanning/manage.py $out/bin/akplanning \
-              --prefix PYTHONPATH : "${pythonPath}" \
-              --prefix PATH : "${path}"
-
-            runHook postInstall
-          '';
-
-        postInstall = ''
-          python $out/lib/akplanning/manage.py collectstatic
-          python $out/lib/akplanning/manage.py compilemessages
-        '';
-
-        nativeCheckInputs = lib.attrValues {
-          inherit (python.pkgs)
-            unittest-xml-reporting
-            beautifulsoup4
-            ;
+        hostName = mkOption {
+          description = "Hostname for the AKPlanning tool";
+          type = types.str;
+          default = "aks.die-koma.org";
         };
 
-        meta = {
-          license = lib.licenses.agpl3Only;
+        extraHostNames = mkOption {
+          description = "Additional hostnames to set up as aliases for the AKPlanning tool";
+          type = types.listOf (types.str);
+          default = [ ];
+        };
+
+        db = mkOption {
+          description = "Database settings for the AKPlanning tool";
+          type = types.submodule {
+            options = {
+              database = mkOption {
+                description = "MariaDB database for the AKPlanning tool";
+                type = types.str;
+                default = "akplanning";
+              };
+
+              username = mkOption {
+                description = "MariaDB username for the AKPlanning tool";
+                type = types.str;
+                default = "akplanning";
+              };
+            };
+          };
+          default = { };
+        };
+
+        nginx = mkEnableOption "configure an nginx VirtualHost for the AKPlanning tool";
+
+        useACMEHost = mkOption {
+          description = "Hostname to use for obtaining SSL certificates";
+          type = types.str;
+          default = config.networking.fqdn;
+        };
+
+        settings = lib.mkOption {
+          description = ''
+            Configuration options to set in `settings.py`.
+          '';
+
+          default = { };
+
+          type = lib.types.submodule {
+            freeformType = pythonFmt.type;
+
+            options = {
+            };
+          };
         };
       };
 
+      config =
+        let
+          cfg = config.die-koma.akplanning;
+          dataDir = "/data/akplanning";
+
+          settingsFile =
+            let
+              inherit (pythonFmt.lib) mkRaw;
+            in
+            pythonFmt.generate "akplanning_settings.py" (
+              {
+                _imports = [ "os" ];
+
+                STATIC_ROOT = "${dataDir}/static";
+                STATICFILES_DIRS = mkRaw ''
+                  (BASE_DIR + "/static_common",)
+                '';
+
+                ALLOWED_HOSTS = [ cfg.hostName ] ++ cfg.extraHostNames;
+                DEBUG = false;
+
+                SESSION_COOKIE_SECURE = true;
+                CSRF_COOKIE_SECURE = true;
+
+                DATABASES.default = {
+                  ENGINE = "django.db.backends.mysql";
+                  HOST = "localhost";
+                  NAME = cfg.db.database;
+                  USER = cfg.db.username;
+                  PASSWORD = mkRaw ''
+                    open(os.getenv("AKPLANNING_DATABASE_PASSWORD")).read()
+                  '';
+                  OPTIONS.init_command = "SET sql_mode='STRICT_TRANS_TABLES'";
+                };
+
+                SEND_MAILS = true;
+                EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend";
+
+                SECRET_KEY = mkRaw ''
+                  open(os.getenv("AKPLANNING_SECRET_KEY")).read()
+                '';
+
+                LOGGING = {
+                  version = 1;
+                  disable_existing_loggers = false;
+                  handlers.console = {
+                    level = "DEBUG";
+                    class = "logging.StreamHandler";
+                  };
+                  root = {
+                    handlers = [ "console" ];
+                    level = "DEBUG";
+                  };
+                };
+              }
+              // cfg.settings
+            );
+          settingsPath = pkgs.stdenv.mkDerivation {
+            pname = "akplanning-settings";
+            version = "0";
+            dontUnpack = true;
+            dontBuild = true;
+            installPhase = ''
+              runHook preInstall
+              mkdir $out
+              echo "from AKPlanning.settings import *" > $out/akplanning_settings.py
+              cat ${settingsFile} >> $out/akplanning_settings.py
+              runHook postInstall
+            '';
+          };
+
+          iniFmt = pkgs.formats.ini { listsAsDuplicateKeys = true; };
+
+          uwsgIni = iniFmt.generate "uwsgi-akplanning.ini" {
+            uwsgi = {
+              plugin = "python3";
+              socket = "/run/akplanning/akplanning.socket";
+              chmod-socket = 660;
+              chdir = dataDir;
+              wsgi-file = "${cfg.package}/lib/akplanning/AKPlanning/wsgi.py";
+              touch-reload = "%(wsgi-file)";
+              env = [
+                "DJANGO_SETTINGS_MODULE=akplanning_settings"
+                "AKPLANNING_SECRET_KEY=/run/secrets/akplanning-secret-key"
+                "AKPLANNING_DATABASE_PASSWORD=/run/secrets/akplanning-database-password"
+              ];
+              processes = 4;
+              threads = 2;
+              uid = "akplanning";
+              gid = "akplanning";
+            };
+          };
+
+          wrapper = pkgs.writeShellApplication {
+            name = "akplanning";
+            runtimeInputs = [ pkgs.coreutils ];
+            text = ''
+              SUDO="exec"
+              if [[ "$USER" != akplanning ]]; then
+                SUDO="exec /run/wrappers/bin/sudo -u akplanning"
+              fi
+              $SUDO env \
+                DJANGO_SETTINGS_MODULE="akplanning_settings" \
+                AKPLANNING_SECRET_KEY="/run/secrets/akplanning-secret-key" \
+                AKPLANNING_DATABASE_PASSWORD="/run/secrets/akplanning-database-password" \
+                PYTHONPATH="${cfg.package.pythonPath}:${cfg.package}/lib/akplanning/:${settingsPath}:${cfg.package.python}/${cfg.package.python.sitePackages}" \
+                ${lib.getExe cfg.package} "$@"
+            '';
+          };
+
+        in
+        {
+          services = {
+            mysql = {
+              ensureDatabases = [ "akplanning" ];
+              ensureUsers = [
+                {
+                  name = "akplanning";
+                  ensurePermissions = {
+                    "akplanning.*" = "ALL PRIVILEGES";
+                  };
+                }
+              ];
+            };
+
+            nginx.virtualHosts = mkIf cfg.nginx {
+              "${cfg.hostName}" = {
+                inherit (cfg) useACMEHost;
+                forceSSL = true;
+                serverAliases = cfg.extraHostNames;
+
+                locations = {
+                  "/" = {
+                    recommendedUwsgiSettings = true;
+                    uwsgiPass = "unix:///run/akplanning/akplanning.socket";
+                  };
+
+                  "^~ /static/" = {
+                    alias = "${dataDir}/static/";
+                    extraConfig = "allow all;";
+                  };
+                };
+              };
+            };
+          };
+
+          systemd = {
+            services.akplanning = {
+              description = "AK planning tool";
+              serviceConfig = {
+                # TODO: check if these are appropriate
+                ReadWritePaths = [ dataDir ];
+                CacheDirectory = "akplanning";
+                CapabilityBoundingSet = "";
+                # ProtectClock adds DeviceAllow=char-rtc r
+                DeviceAllow = "";
+                LockPersonality = true;
+                MemoryDenyWriteExecute = true;
+                NoNewPrivileges = true;
+                PrivateDevices = true;
+                PrivateMounts = true;
+                PrivateTmp = true;
+                PrivateUsers = true;
+                ProtectClock = true;
+                ProtectHome = true;
+                ProtectHostname = true;
+                ProtectSystem = "strict";
+                ProtectControlGroups = true;
+                ProtectKernelLogs = true;
+                ProtectKernelModules = true;
+                ProtectKernelTunables = true;
+                ProtectProc = "invisible";
+                ProcSubset = "pid";
+                RestrictAddressFamilies = [
+                  "AF_UNIX"
+                  "AF_INET"
+                  "AF_INET6"
+                ];
+                RestrictNamespaces = true;
+                RestrictRealtime = true;
+                RestrictSUIDSGID = true;
+                SystemCallArchitectures = "native";
+                SystemCallFilter = [
+                  "@system-service"
+                  "~@privileged @setuid @keyring"
+                ];
+                UMask = "0066";
+
+                WorkingDirectory = dataDir;
+                User = "akplanning";
+                Group = "akplanning";
+                TimeoutStartSec = "5m";
+              };
+
+              after = [
+                "mysql.service"
+                "network.target"
+              ];
+              wantedBy = [ "multi-user.target" ];
+              environment = {
+                DJANGO_SETTINGS_MODULE = "akplanning_settings";
+                AKPLANNING_SECRET_KEY = "/run/secrets/akplanning-secret-key";
+                AKPLANNING_DATABASE_PASSWORD = "/run/secrets/akplanning-database-password";
+                PYTHONPATH = "${cfg.package.pythonPath}:${cfg.package}/lib/akplanning/:${settingsPath}:${cfg.package.python}/${cfg.package.python.sitePackages}";
+              };
+              script =
+                let
+                  uwsgi = pkgs.uwsgi.override { plugins = [ "python3" ]; };
+                in
+                ''
+                  ${lib.getExe uwsgi} \
+                    --ini ${uwsgIni} \
+                    --log-4xx \
+                    --log-5xx \
+                    --log-zero
+                '';
+              preStart = ''
+                ${lib.getExe cfg.package} migrate --no-input
+                ${lib.getExe cfg.package} collectstatic --no-input --clear
+              '';
+            };
+
+            tmpfiles.rules = [ "d /run/akplanning - akplanning akplanning - -" ];
+          };
+
+          environment.systemPackages = [ wrapper ];
+
+          users = {
+            users = {
+              akplanning = {
+                home = dataDir;
+                homeMode = "0750";
+                createHome = true;
+                isSystemUser = true;
+                group = "akplanning";
+              };
+              nginx = mkIf cfg.nginx { extraGroups = [ "akplanning" ]; };
+            };
+            groups.akplanning = { };
+          };
+        };
     };
 }
---
+++ modules/packages/akplanning.nix
@@ -0,0 +1,153 @@
+{
+  lib,
+  ...
+}:
+
+{
+  perSystem =
+    {
+      self',
+      pkgs,
+      ...
+    }:
+
+    let
+      src = pkgs.fetchgit {
+        url = "https://gitlab.fachschaften.org/kif/akplanning.git";
+        rev = "faf3e534998f67e315cfa0f9c5d21b87c032a632";
+        hash = "sha256-rmnxA2xj6xXUebzottnzkaAF091KaT2b1Pwi2frGZQ4=";
+      };
+
+      python = pkgs.python3Packages.python.override {
+        packageOverrides = self: super: {
+          django = super.django_5;
+
+          inherit (self'.packages)
+            django-admin-logs
+            django-betterforms
+            django-bootstrap-datepicker-plus
+            django-docs
+            django-fontawesome-6
+            django-tex
+            django-registration-redux
+            sphinxcontrib-django
+            ;
+        };
+      };
+
+      texlive = pkgs.texlive.combine {
+        inherit (pkgs.texlive.pkgs)
+          collection-basic
+          collection-luatex
+          collection-latex
+          collection-latexrecommended
+          collection-latexextra
+          collection-fontsrecommended
+          collection-fontsextra
+          collection-langgerman
+          beamer
+          ;
+      };
+
+      path = lib.makeBinPath [
+        texlive
+      ];
+
+      dependencies = lib.attrValues {
+        inherit (python.pkgs)
+          django
+          django-admin-logs
+          django-betterforms
+          django-bootstrap-datepicker-plus
+          django-bootstrap5
+          django-compressor
+          django-debug-toolbar
+          django-fontawesome-6
+          django-libsass
+          django-registration-redux
+          django-simple-history
+          django-split-settings
+          django-tex
+          django-timezone-field
+          django-csp
+          djangorestframework
+
+          fontawesomefree
+          mysqlclient
+          tzdata
+          jsonschema
+
+          sphinx
+          sphinx-rtd-theme
+          sphinxcontrib-apidoc
+          sphinxcontrib-django
+          recommonmark
+          django-docs
+          ;
+      };
+
+      pythonPath = python.pkgs.makePythonPath dependencies;
+    in
+    {
+      devShells.akplanning = pkgs.mkShell {
+        inputsFrom = self'.packages.aktool;
+      };
+
+      packages.akplanning = python.pkgs.buildPythonApplication {
+        name = "akplanning";
+        version = "0-unstable-2026-05-18";
+        pyproject = false;
+        inherit src;
+
+        nativeBuildInputs = [
+          pkgs.gettext
+        ];
+
+        inherit dependencies;
+
+        buildPhase = ''
+          runHook preBuild
+
+          mkdir -p $out/lib
+          cp -r $src $out/lib/akplanning
+          chmod -R +w $out/lib/akplanning
+          rm -rf $out/lib/akplanning/docs
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          chmod +x $out/lib/akplanning/manage.py
+          makeWrapper $out/lib/akplanning/manage.py $out/bin/akplanning \
+            --prefix PYTHONPATH : "${pythonPath}" \
+            --prefix PATH : "${path}"
+
+          runHook postInstall
+        '';
+
+        postInstall = ''
+          python $out/lib/akplanning/manage.py collectstatic
+          python $out/lib/akplanning/manage.py compilemessages
+        '';
+
+        nativeCheckInputs = lib.attrValues {
+          inherit (python.pkgs)
+            unittest-xml-reporting
+            beautifulsoup4
+            ;
+        };
+
+        passthru = {
+          inherit python pythonPath;
+        };
+
+        meta = {
+          license = lib.licenses.agpl3Only;
+          mainProgram = "akplanning";
+        };
+      };
+
+    };
+}
